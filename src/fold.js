a191f8126cb3f130786ebc7460dd691f7111ed46
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-12-17 15:14:57 +0100

    Added calcBarWidth function to fold.js

--- src/fold.js
+++ src/fold.js
@@ -12,8 +12,12 @@ var foldInfo = {
   add: function(foldData, addon) {
     // Calculate the lines we will fold.
     var range = foldData.data.end.row-foldData.data.start.row;
+
+    var width = this.calcBarWidth(editor.getSession().doc.$lines.length, range, 50);
+    //console.log(width);
+
     // add the range to the addon cell
-    addon.setGutterCell(foldData.data.end.row, 'sloc: '+(range-1));
+    addon.setGutterCell(foldData.data.end.row, '<div class="sloc_cell" style="background-color:rgba(0, 71, 255, 0.31); width:'+width+'px; height:16px; position:absolute"></div>sloc: '+(range-1));
   },
 
   /**
@@ -24,6 +28,13 @@ var foldInfo = {
    */
   remove: function(foldData, addon) {
     addon.setGutterCell(foldData.data.end.row, '');
-  }
+  },
 
+  calcBarWidth: function(totalLoc, foldedLoc, width) {
+    // console.log('totalLoc', totalLoc);
+    // console.log('foldedLoc', foldedLoc);
+    var tmp = width / totalLoc;
+    var result = tmp * (foldedLoc+2);
+    return result;
+  }
 };
